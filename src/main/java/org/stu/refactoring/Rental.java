754f499eefcf26892212da141407b6337084980a
shiying.ke <shiying.ke@geely.com>
2019-11-12 00:14:02 +0800

    Move Method（搬移函数）,把根据影片类型而变化的东西，getCharge(),getFrequentRenterPoints()，搬移到影片类型所属的类中

--- src/main/java/org/stu/refactoring/Rental.java
+++ src/main/java/org/stu/refactoring/Rental.java
@@ -25,31 +25,10 @@ public class Rental {
     }
 
     double getCharge() {
-        double result = 0;
-        // 算出每行的金额
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+        return movie.getCharge(daysRented);
     }
 
     int getFrequentRenterPoints() {
-        // 租赁超过两天新影片的额外积分
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                getDaysRented() > 1)
-            return 2;
-        return 1;
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
