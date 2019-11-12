f6ba7ecb8482199ddf4ef356506e5cc470afd586
shiying.ke <shiying.ke@geely.com>
2019-11-12 11:29:32 +0800

    Replace Conditional with Polymorphism（以多态取代条件表达式），重构Price.getCharge()

--- src/main/java/org/stu/refactoring/NewReleasePrice.java
+++ src/main/java/org/stu/refactoring/NewReleasePrice.java
@@ -10,4 +10,9 @@ public class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    @Override
+    double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
 }
--- src/main/java/org/stu/refactoring/Price.java
+++ src/main/java/org/stu/refactoring/Price.java
@@ -9,24 +9,5 @@ package org.stu.refactoring;
 public abstract class Price {
     abstract int getPriceCode();
 
-    double getCharge(int daysRented) {
-        double result = 0;
-        // 算出每行的金额
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
+    abstract double getCharge(int daysRented);
 }
